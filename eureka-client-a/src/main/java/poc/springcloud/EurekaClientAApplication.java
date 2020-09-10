936bce774ee32cada8df8b51c8bb3c37885c5938
ashishb888 <ashish.bhosle008@gmail.com>
2020-09-10 09:55:52 +0530

    eureka-client-a: Initial commit

---
+++ eureka-client-a/src/main/java/poc/springcloud/EurekaClientAApplication.java
@@ -0,0 +1,13 @@
+package poc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientAApplication.class, args);
+	}
+
+}
---
+++ eureka-client-a/src/test/java/poc/springcloud/EurekaClientAApplicationTests.java
@@ -0,0 +1,13 @@
+package poc.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
